c2df0625a475d23fde57e9b53fae4eaf9c87ddab
Max Kelly <maxwill.kelly@gmail.com>
2026-04-27 20:53:45 +0100

    fix: Move social buttons to left side

--- src/app/_components/HeroSection.tsx
+++ src/app/_components/HeroSection.tsx
@@ -9,27 +9,19 @@ import { SocialIcon } from "./hero/SocialIcon";
 export const HeroSection = () => {
   return (
     <section className="flex items-center justify-between gap-6" id="hero">
-      <div className="flex-1">
-        <DiaTextReveal
-          as="h1"
-          className="text-6xl font-bold"
-          text="Max Kelly"
-        />
-        <DiaTextReveal
-          as="p"
-          className="text-lg font-light mt-4"
-          text="I'm a Software Engineer that builds interactive applications with an eye for small details."
-        />
-      </div>
-      <div className="flex flex-col items-center gap-3">
-        <Image
-          alt="Max Kelly"
-          className="size-24 shrink-0 rounded-xl object-cover sm:size-32"
-          height={400}
-          priority
-          src="/profile.jpg"
-          width={400}
-        />
+      <div className="flex flex-1 flex-col gap-4">
+        <div>
+          <DiaTextReveal
+            as="h1"
+            className="text-6xl font-bold"
+            text="Max Kelly"
+          />
+          <DiaTextReveal
+            as="p"
+            className="text-lg font-light mt-4"
+            text="I'm a Software Engineer that builds interactive applications with an eye for small details."
+          />
+        </div>
         <div className="flex gap-2">
           <SocialIcon
             ariaLabel="Download CV"
@@ -52,6 +44,14 @@ export const HeroSection = () => {
           />
         </div>
       </div>
+      <Image
+        alt="Max Kelly"
+        className="size-24 shrink-0 rounded-xl object-cover sm:size-32"
+        height={400}
+        priority
+        src="/profile.jpg"
+        width={400}
+      />
     </section>
   );
 };
