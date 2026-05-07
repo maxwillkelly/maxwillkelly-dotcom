3d3f7ad1e26c8b8fd0f8d4763e0cee43e60e277d
Max Kelly <maxwill.kelly@gmail.com>
2026-05-07 22:31:02 +0100

    feat: Additional experience

--- src/app/_components/ExperienceSection.tsx
+++ src/app/_components/ExperienceSection.tsx
@@ -35,12 +35,43 @@ const experiences: TimelineEntry[] = [
         , the leading online service for school governors and trustees.
         <br />
         <br />
+        I have become the most senior developer on the team. In addition to
+        building software, I've had the opportunity to onboard and train new
+        engineers. I work with product managers and other stakeholders to write
+        technical specifications and gather requirements.
+        <br />
+        <br />
         My colleagues and I were responsible for a complete ground up rewrite of
         GovernorHub's front-end. At the time I joined, GovernorHub was a legacy
         AngularJS codebase with React injected into it for specific components.
         We completely rearchitected it using the Next.js App router which
         produced a much better user experience. I was responsible for
         reimplementing many significant features in React.
+        <br />
+        <br />I built processes to triage bugs from customer feedback in
+        Intercom and convert them into issues in Linear. I've worked on
+        integrations with products build by other teams internally and
+        externally such as{" "}
+        <Link
+          className="text-base no-underline hover:underline"
+          href="https://www.salesforce.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Salesforce
+          <Link.Icon />
+        </Link>{" "}
+        and{" "}
+        <Link
+          className="text-base no-underline hover:underline"
+          href="https://get-information-schools.service.gov.uk/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Get Information About Schools (GIAS)
+          <Link.Icon />
+        </Link>{" "}
+        .
         <br />
         <br />
         I've also had the opportunity to onboard new developers into an
